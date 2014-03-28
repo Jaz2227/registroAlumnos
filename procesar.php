014e82369be86aca90bccad73d8d58665be487c5
Jaz2227 <chiva368@hotmail.com>
2014-03-28 12:10:57 -0600

    registroAlumnos
    practica

---
+++ procesar.php
@@ -0,0 +1,21 @@
+﻿<?php
+require 'conexion.php';
+
+$nom=$_POST["Nom"];
+$semestre=$_POST["Semestre"];
+$cal1=$_POST["Cal1"];
+$cal2=$_POST["Cal2"];
+$cal3=$_POST["Cal3"];
+$cal4=$_POST["Cal4"];
+
+$sql="INSERT INTO registro(id_alumno, nom, semestre, cal1, cal2, cal3, cal4)VALUES(' ','$nombre','$semestre','$cal1','$cal2','$cal3','$cal4')";
+$resultado=mysql_query($sql,$con);
+
+if($resultado){
+ echo "El registro se almaceno de manera correcta: ".$nombre." Mail: ".$mail." Telefono: ".$telefono;    
+}else{
+ die("Error de conexión".mysql_error());   
+}
+
+mysql_close($con);
+?>
